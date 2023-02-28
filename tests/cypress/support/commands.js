df79faee26339b34b02f81d15c089db020d02583
laryssaR-QA <laryssafernandar@hotmail.com>
2023-02-28 15:04:33 -0300

    back & front

---
+++ tests/cypress/support/commands.js
@@ -0,0 +1,21 @@
+//COMMANDS em pages
+// Cypress.Commands.add('login', (user)=> {
+//     cy.visit('/')
+//     if (user.instagram) cy.get('input[name=instagram]').type(user.instagram)//se conter campo preenchido executa o cod
+//     if (user.password) cy.get('input[name=password]').type(user.password)
+  
+//     cy.contains('button', 'Entrar').click()
+//   })
+  
+  // Cypress.Commands.add('modalHaveText', (Text)=>{
+  //   cy.get('.swal2-html-container')//class
+  //     .should('be.visible')
+  //     .should('have.text', Text)
+  // })
+  
+  // Cypress.Commands.add('loggedUser', (name)=> {
+  //   cy.get('.logged-user')//class
+  //   .should('be.visible')
+  //   .should('have.text', `Olá, ${name}`)
+  
+  // })
